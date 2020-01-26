8367c54a9d66388e58ffe7e75ffca7c71db27b5f
protolambda <proto@protolambda.com>
2020-01-26 05:23:31 +0100

    kdht and disv5 support

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module eth2-lurk
 go 1.13
 
 require (
+	github.com/ethereum/go-ethereum v1.9.10
 	github.com/libp2p/go-libp2p v0.5.1
 	github.com/libp2p/go-libp2p-core v0.3.0
+	github.com/libp2p/go-libp2p-kad-dht v0.5.0
 	github.com/libp2p/go-libp2p-pubsub v0.2.5
-	github.com/libp2p/go-libp2p-secio v0.2.1
 )
--- main.go
+++ main.go
@@ -3,23 +3,33 @@ package main
 import (
 	"bytes"
 	"context"
+	"crypto/ecdsa"
 	"encoding/base64"
 	"encoding/hex"
 	"fmt"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/p2p/discv5"
+	"github.com/ethereum/go-ethereum/p2p/enode"
+	"github.com/ethereum/go-ethereum/p2p/enr"
+	"github.com/ethereum/go-ethereum/rlp"
+	ds "github.com/ipfs/go-datastore"
+	ds_sync "github.com/ipfs/go-datastore/sync"
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/protocol"
+	kad_dht "github.com/libp2p/go-libp2p-kad-dht"
+	dhtopts "github.com/libp2p/go-libp2p-kad-dht/opts"
 	"github.com/libp2p/go-libp2p-pubsub"
 	pubsub_pb "github.com/libp2p/go-libp2p-pubsub/pb"
-	"io"
-	"log"
-	"path"
-	//"github.com/libp2p/go-libp2p-secio"
 	"github.com/libp2p/go-tcp-transport"
 	"github.com/minio/sha256-simd"
 	ma "github.com/multiformats/go-multiaddr"
+	"io"
+	"net"
 	"os"
 	"os/signal"
+	"path"
 	"syscall"
 	"time"
 )
@@ -48,7 +58,7 @@ func main() {
 		if err != nil {
 			return err
 		}
-		errLogger := NewErrLogger(ctx, outName, os.Stderr)
+		errLogger := lu.NewErrLogger(ctx, outName)
 		msgLogger := NewMessageLogger(ctx, out, errLogger)
 		return lu.LurkTopic(ctx, topic, msgLogger, errLogger)
 	}
@@ -61,9 +71,65 @@ func main() {
 
 	// Connect with peers after the pubsub is all set up,
 	// so that peers do not have to learn about pubsub interest after being connected.
+
+	// TODO: enode list option -> bootnodes discv5
+	// TODO: multi addr list option -> bootnodes kdht
+
+	// kademlia
+	{
+		if err := lu.InitKadDHT("/prysm/0.0.0/dht"); err != nil {
+			panic(err)
+		}
+		bootAddrStrs := []string{
+			"/dns4/prylabs.net/tcp/30001/p2p/16Uiu2HAm7Qwe19vz9WzD2Mxn7fXd1vgHHp4iccuyq7TxwRXoAGfc",
+		}
+		bootNodes := make([]ma.Multiaddr, 0, len(bootAddrStrs))
+		for _, addr := range bootAddrStrs {
+			muAddr, err := ma.NewMultiaddr(addr)
+			if err != nil {
+				panic(err)
+			}
+			bootNodes = append(bootNodes, muAddr)
+		}
+		if err := lu.AddKadBootNodes(bootNodes); err != nil {
+			panic(err)
+		}
+	}
+
+	// disc v5
+	if false { // disabled for now
+		// TODO configure udp addr and keypair
+		if err := lu.InitDiscV5("", nil); err != nil {
+			panic(err)
+		}
+		bootAddrStrs := []string{
+			".........", // TODO base64 enr with udp port
+		}
+		bootNodes := make([]*discv5.Node, 0, len(bootAddrStrs))
+		for _, addr := range bootAddrStrs {
+			enrRec, err := parseEnr(addr)
+			if err != nil {
+				panic(err)
+			}
+			enodeAddr, err := enrToEnode(enrRec, true)
+			if err != nil {
+				panic(err)
+			}
+			dv5Node, err := enodeToDiscv5Node(enodeAddr)
+			if err != nil {
+				panic(err)
+			}
+			bootNodes = append(bootNodes, dv5Node)
+		}
+		if err := lu.AddDiscV5BootNodes(bootNodes); err != nil {
+			panic(err)
+		}
+	}
+
+	// static peers
 	if err := lu.ConnectStaticPeers([]ma.Multiaddr{
 		// TODO connect with peers
-	}); err != nil {
+	}, nil); err != nil {
 		panic(err)
 	}
 
@@ -102,6 +168,8 @@ func (mCtx *ManagedCtx) NewSubCtx() *ManagedCtx {
 type Lurker struct {
 	ManagedCtx
 
+	log log.Logger
+
 	host    host.Host
 	hostCtx *ManagedCtx
 
@@ -110,6 +178,13 @@ type Lurker struct {
 
 	connectionsCtx *ManagedCtx
 	connections    map[peer.ID]*ManagedCtx
+
+	kDht    *kad_dht.IpfsDHT
+	kDhtCtx *ManagedCtx
+
+	dv5Net  *discv5.Network
+	dv5Ctx  *ManagedCtx
+	dv5Log  log.Logger
 }
 
 func NewLurker(ctx context.Context) (*Lurker, error) {
@@ -136,9 +211,118 @@ func (lu *Lurker) openHost() error {
 	return err
 }
 
-// TODO add KDHT and discV5 for more peers
+func (lu *Lurker) AddKadBootNodes(bootAddrs []ma.Multiaddr) error {
+	return lu.ConnectStaticPeers(bootAddrs, func(peerInfo peer.AddrInfo, alreadyConnected bool) error {
+		// protect the peer, we don't want the peer-limit to mess with the bootnodes when pruning.
+		lu.host.ConnManager().Protect(peerInfo.ID, "bootnode")
+		return nil
+	})
+}
 
-func (lu *Lurker) ConnectStaticPeers(multiAddrs []ma.Multiaddr) error {
+func (lu *Lurker) InitKadDHT(id protocol.ID) error {
+	// example protocol id: "/prysm/0.0.0/dht"
+	dhtOpts := []dhtopts.Option{
+		dhtopts.Datastore(ds_sync.MutexWrap(ds.NewMapDatastore())),  // instead of the default map datastore.
+		dhtopts.Protocols(id),  // don't creep onto the default IPFS network, join the configured DHT
+	}
+	kdCtx := lu.NewSubCtx()
+	kd, err := kad_dht.New(lu.kDhtCtx.ctx, lu.host, dhtOpts...)
+	if err != nil {
+		return err
+	}
+	lu.kDhtCtx = kdCtx
+	lu.kDht = kd
+	return nil
+}
+
+func (lu *Lurker) RefresKadTable() {
+	refResult := lu.kDht.RefreshRoutingTable()
+
+	// Result is safe to ignore but interesting to log.
+	go func() {
+		err := <-refResult
+		if err != nil {
+			fmt.Printf("failed to refresh kad dht table: %v", err)
+		} else {
+			fmt.Println("successfully refreshed kad dht table")
+		}
+	}()
+}
+
+func (lu *Lurker) InitDiscV5(addr string, privKey *ecdsa.PrivateKey) error {
+	dv5Log := lu.log.New("discv5")
+
+	udpAddr, err := net.ResolveUDPAddr("udp", addr)
+	if err != nil {
+		return err
+	}
+
+	conn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		return err
+	}
+	dv5Log.Debug("UDP listener up", "addr", udpAddr)
+
+	dv5Net, err := discv5.ListenUDP(privKey, conn, "", nil)
+	if err != nil {
+		return err
+	}
+	dv5Log.Debug("Discv5 listener up", "addr", udpAddr)
+
+	dv5Ctx := lu.NewSubCtx()
+	go func() {
+		<-dv5Ctx.ctx.Done()
+		dv5Net.Close()
+	}()
+
+	lu.dv5Net = dv5Net
+	lu.dv5Ctx = dv5Ctx
+	lu.dv5Log = dv5Log
+	return nil
+}
+
+func (lu *Lurker) AddDiscV5BootNodes(bootNodes []*discv5.Node) error {
+	return lu.dv5Net.SetFallbackNodes(bootNodes)
+}
+
+func parseEnr(v string) (*enr.Record, error) {
+	data, err := base64.RawURLEncoding.DecodeString(v)
+	if err != nil {
+		return nil, err
+	}
+	var record enr.Record
+	if err := rlp.Decode(bytes.NewReader(data), &record); err != nil {
+		return nil, err
+	}
+	return &record, nil
+}
+
+func enrToEnode(record *enr.Record, verifySig bool) (*enode.Node, error) {
+	idSchemeName := record.IdentityScheme()
+
+	if verifySig {
+		if err := record.VerifySignature(enode.ValidSchemes[idSchemeName]); err != nil {
+			return nil, err
+		}
+	}
+
+	return enode.New(enode.ValidSchemes[idSchemeName], record)
+}
+
+func enodeToDiscv5Node(en *enode.Node) (*discv5.Node, error) {
+	id := discv5.PubkeyID(en.Pubkey())
+	ip := en.IP()
+	udpPort, tcpPort := uint16(en.UDP()), uint16(en.TCP())
+	if ip == nil || udpPort == 0 || tcpPort == 0 {
+		return nil, fmt.Errorf("enode record %v has missing ip/udp/tcp", en.String())
+	}
+	return &discv5.Node{IP:  ip, UDP: udpPort, TCP: tcpPort, ID:  id}, nil
+}
+
+type ConnectionCallback func(info peer.AddrInfo, alreadyConnected bool) error
+
+// onConnect may be nil, if no further action is required after starting the connection.
+func (lu *Lurker) ConnectStaticPeers(multiAddrs []ma.Multiaddr, onConnect ConnectionCallback) error {
 	infos, err := peer.AddrInfosFromP2pAddrs(multiAddrs...)
 	if err != nil {
 		return err
@@ -147,10 +331,18 @@ func (lu *Lurker) ConnectStaticPeers(multiAddrs []ma.Multiaddr) error {
 		lu.connectionsCtx = lu.NewSubCtx()
 	}
 	for _, info := range infos {
-		peerCtx := lu.connectionsCtx.NewSubCtx()
-		lu.connections[info.ID] = peerCtx
-		if err := lu.host.Connect(peerCtx.ctx, info); err != nil {
-			return err
+		alreadyConnected := lu.connections[info.ID] != nil
+		if !alreadyConnected {
+			peerCtx := lu.connectionsCtx.NewSubCtx()
+			lu.connections[info.ID] = peerCtx
+			if err := lu.host.Connect(peerCtx.ctx, info); err != nil {
+				return err
+			}
+		}
+		if onConnect != nil {
+			if err := onConnect(info, alreadyConnected); err != nil {
+				return err
+			}
 		}
 	}
 	return nil
@@ -241,14 +433,14 @@ func NewMessageLogger(ctx context.Context, w io.Writer, outErr chan<- error) cha
 	return listenerCh
 }
 
-func NewErrLogger(ctx context.Context, name string, w io.Writer) chan<- error {
-	l := log.New(w, name+": ", log.LUTC|log.Ldate)
+func (lu *Lurker) NewErrLogger(ctx context.Context, name string) chan<- error {
+	l := lu.log.New(name)
 	listenerCh := make(chan error)
 	go func() {
 		for {
 			select {
 			case msg := <-listenerCh:
-				l.Println(msg)
+				l.Error(msg.Error())
 			case <-ctx.Done():
 				return
 			}
