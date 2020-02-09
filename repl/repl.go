e39b30edaace468ac84b2ff59a4abb6131b88e9e
protolambda <proto@protolambda.com>
2020-02-09 20:04:47 +0100

    host repl commands

--- repl/repl.go
+++ repl/repl.go
@@ -2,19 +2,38 @@ package repl
 
 import (
 	"context"
+	"crypto/rand"
+	"encoding/hex"
 	"eth2-lurk/node"
+	"eth2-lurk/peering/static"
 	"fmt"
+	"github.com/libp2p/go-libp2p"
+	connmgr "github.com/libp2p/go-libp2p-connmgr"
+	"github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/libp2p/go-libp2p-core/host"
+	mplex "github.com/libp2p/go-libp2p-mplex"
+	"github.com/libp2p/go-libp2p-peerstore/pstoremem"
+	secio "github.com/libp2p/go-libp2p-secio"
+	yamux "github.com/libp2p/go-libp2p-yamux"
+	"github.com/libp2p/go-tcp-transport"
+	ws "github.com/libp2p/go-ws-transport"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"strings"
+	"time"
 )
 
 type Repl struct {
-	HostNode *node.Node
-	Log   logrus.FieldLogger
-	Ctx   context.Context
-	Cancel func()
+	P2PHost host.Host
+	PrivLibp2pRSA crypto.PrivKey
+	Log     logrus.FieldLogger
+	Ctx     context.Context
+	Cancel  func()
 	ReplCmd *cobra.Command
 }
+
+// check interface
+var _ = (node.Node)((*Repl)(nil))
 
 func NewRepl(log logrus.FieldLogger) *Repl {
 	repl := &Repl{
@@ -46,8 +65,28 @@ func NewRepl(log logrus.FieldLogger) *Repl {
 	return repl
 }
 
-func writeErr(cmd *cobra.Command, format string, a ...interface{}) {
-	_, _ = fmt.Fprintf(cmd.ErrOrStderr(), format, a)
+func (r *Repl) Host() host.Host {
+	return r.P2PHost
+}
+
+func (r *Repl) Logger(logTopic string) logrus.FieldLogger {
+	return r.Log.WithField("log_topic", logTopic)
+}
+
+func writeErrMsg(cmd *cobra.Command, format string, a ...interface{}) {
+	_, _ = fmt.Fprintf(cmd.ErrOrStderr(), format + "\n", a)
+}
+
+func writeErr(cmd *cobra.Command, err error) {
+	_, _ = fmt.Fprintf(cmd.ErrOrStderr(), err.Error() + "\n")
+}
+
+func (r *Repl) NoHost(cmd *cobra.Command) bool {
+	if r.P2PHost == nil {
+		writeErrMsg(cmd, "REPL must have initialized Libp2p host. Try 'host start'")
+		return true
+	}
+	return false
 }
 
 func (r *Repl) InitHostCmd() *cobra.Command {
@@ -55,29 +94,141 @@ func (r *Repl) InitHostCmd() *cobra.Command {
 		Use:   "host",
 		Short: "Manage host",
 	}
-	cmd.AddCommand(&cobra.Command{
-		Use:   "start [--priv-key=...] [--security=<secio|tls|noise>] [--mux=<yamux-mplex|mplex|yamux>] [--transports=<tcp,ws>]",
+	var privKeyStr string
+	var transportsStrArr []string
+	var muxStrArr []string
+	var securityStr string
+	var relayEnabled bool
+
+	startCmd := &cobra.Command{
+		Use:   "start [--priv-key=...] [--security=<secio|tls|noise>] [--mux=yamux,mplex] [--transports=tcp,ws]",
 		Short: "Start the host node",
 		Run: func(cmd *cobra.Command, args []string) {
-			if r.HostNode != nil {
-				writeErr(cmd, "Already have a host open.")
+			if r.P2PHost != nil {
+				writeErrMsg(cmd, "Already have a host open.")
 				return
 			}
+			{
+				if privKeyStr == "" { // generate new private key if non was specified
+					var err error
+					r.PrivLibp2pRSA, _, err = crypto.GenerateKeyPairWithReader(crypto.RSA, 2048, rand.Reader)
+					if err != nil {
+						writeErr(cmd, err)
+						return
+					}
+				} else {
+					privKeyBytes, err := hex.DecodeString(privKeyStr)
+					if err != nil {
+						writeErrMsg(cmd, "cannot parse private key, expected hex string (without 0x)")
+						return
+					}
+					r.PrivLibp2pRSA, err = crypto.UnmarshalRsaPrivateKey(privKeyBytes)
+					if err != nil {
+						writeErrMsg(cmd, "cannot parse private key, invalid RSA key")
+						return
+					}
+				}
+			}
+			hostOptions := make([]libp2p.Option, 0)
 
+			for _, v := range transportsStrArr {
+				v = strings.ToLower(strings.TrimSpace(v))
+				switch v {
+				case "tcp":
+					hostOptions = append(hostOptions, libp2p.Transport(tcp.NewTCPTransport))
+				case "ws":
+					hostOptions = append(hostOptions, libp2p.Transport(ws.New))
+				default:
+					writeErrMsg(cmd, "could not recognize transport %s", v)
+					return
+				}
+			}
+
+			for _, v := range muxStrArr {
+				v = strings.ToLower(strings.TrimSpace(v))
+				switch v {
+				case "yamux":
+					hostOptions = append(hostOptions, libp2p.Muxer("/yamux/1.0.0", yamux.DefaultTransport))
+				case "mplex":
+					hostOptions = append(hostOptions, libp2p.Muxer("/mplex/6.7.0", mplex.DefaultTransport))
+				default:
+					writeErrMsg(cmd, "could not recognize mux %s", v)
+					return
+				}
+			}
+
+			{
+				switch securityStr {
+				case "none":
+					// no security, for debugging etc.
+				case "secio":
+					hostOptions = append(hostOptions, libp2p.Security(secio.ID, secio.New))
+				default:
+					writeErrMsg(cmd, "could not recognize security %s", securityStr)
+					return
+				}
+			}
+
+			if relayEnabled {
+				hostOptions = append(hostOptions, libp2p.EnableRelay())
+			}
+			hostOptions = append(hostOptions,
+				libp2p.Identity(r.PrivLibp2pRSA),
+				libp2p.Peerstore(pstoremem.NewPeerstore()), // TODO: persist peerstore?
+				libp2p.ConnectionManager(connmgr.NewConnManager(15, 20, time.Second*20)),
+			)
+			h, err := libp2p.New(r.Ctx, hostOptions...)
+			if err != nil {
+				writeErr(cmd, err)
+				return
+			}
+			r.P2PHost = h
 		},
-	})
-	cmd.AddCommand(&cobra.Command{
-		Use:   "listen [multi-addr [multi-addr [...]]]",
-		Short: "Start listening on given addresses",
-		Run: func(cmd *cobra.Command, args []string) {
+	}
+	startCmd.Flags().StringVar(&privKeyStr, "priv-key", "", "hex-encoded RSA private key for libp2p host. Random if none is specified.")
+	startCmd.Flags().StringArrayVar(&muxStrArr, "mux", []string{"yamux", "mplex"}, "Multiplexers to use")
+	startCmd.Flags().StringArrayVar(&transportsStrArr, "transports", []string{"tcp"}, "Transports to use. Options: tcp, ws")
+	startCmd.Flags().StringVar(&securityStr, "security", "secio", "Security to use. Options: secio, none")
+	startCmd.Flags().BoolVar(&relayEnabled, "relay", false, "enable relayer functionality")
 
+	cmd.AddCommand(startCmd)
+	cmd.AddCommand(&cobra.Command{
+		Use:   "listen <multi-addr> [multi-addr [multi-addr [...]]]",
+		Short: "Start listening on given addresses",
+		Args: cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			if r.NoHost(cmd) {
+				return
+			}
+			maddrs, err := static.ParseMultiAddrs(args...)
+			if err != nil {
+				writeErr(cmd, err)
+				return
+			}
+			if err := r.Host().Network().Listen(maddrs...); err != nil {
+				writeErr(cmd, err)
+				return
+			}
 		},
 	})
 	cmd.AddCommand(&cobra.Command{
 		Use:   "view",
 		Short: "View local peer ID, listening addresses, etc.",
 		Run: func(cmd *cobra.Command, args []string) {
-
+			if r.NoHost(cmd) {
+				return
+			}
+			log := r.Logger("host")
+			h := r.Host()
+			log.Infof("Peer ID: %s", h.ID().Pretty())
+			for _, a := range h.Addrs() {
+				log.Infof("Listening on: %s", a.String())
+			}
+			log.Infof("Security: %s,  Mux: %s,  Transports: %s,  Relay: %v",
+				strings.ToLower(securityStr),
+				strings.ToLower(strings.Join(muxStrArr, ", ")),
+				strings.ToLower(strings.Join(transportsStrArr, ", ")),
+				relayEnabled)
 		},
 	})
 	return cmd
