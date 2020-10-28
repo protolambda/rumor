d7f9032ca0e5ff2153962512abd1aefd3cb81356
Cortze <mikelcortes013@gmail.com>
2020-10-28 10:50:21 +0100

    Solved problem on the 'gossip list' command, previously just showing 1 item

--- control/actor/gossip/list.go
+++ control/actor/gossip/list.go
@@ -21,7 +21,7 @@ func (c *GossipListCmd) Run(ctx context.Context, args ...string) error {
 	topics := make([]string, 0)
 	c.GossipState.Topics.Range(func(key, value interface{}) bool {
 		topics = append(topics, key.(string))
-		return false
+		return true
 	})
 	c.Log.WithField("topics", topics).Infof("On %d topics.", len(topics))
 	return nil
